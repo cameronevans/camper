760b9eb5d6ce5d482d403e4815e0feb227ffb67d
Evans, Cameron <C.Evans@cvent.com>
2017-12-10 09:34:41 -0400

    added webpack, install polyfill from npm, general tidy-up

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+const browser = require('webextension-polyfill');
+const path = require('path');
+
 let state = {
   album: {},
   tabId: null
@@ -15,31 +18,27 @@ function sanitize(str) {
   return str.replace(/[\\\/$'"]/g, '');
 }
 
-function download(message) {
-  console.log('message: ', message);
-  console.log('downloading', state.album);
+function download(message, sender) {
   const {album} = state;
   const folder = `${album.artist} - ${album.title}`;
   album.tracks.forEach(({track_num: number, title, file: {'mp3-128': url}}) => {
     const filename = `${sanitize(folder)}/${sanitize(title)}.mp3`;
     let downloading = browser.downloads.download({filename, url});
     downloading.then(onStartedDownload, onFailed);
-  }); // put sendresponse in callback?
-  
+  });
+
   return true;
 }
 
-// show API method doesn't seem to support the callback parameter,
-// falling back to call it without a callback:
-// Error: Invocation of form pageAction.show(integer, function) doesn't match definition pageAction.show(integer tabId)
-
 function onReceiveAlbum(album) {
-  console.log('receiving album', state.album);
   if (Object.keys(state.album).length === 0) {
-    console.log('received');
     state.album = album;
-    console.log(state.tabId);
-    browser.pageAction.show(state.tabId);
+    if (window.chrome) {
+      // waiting for https://github.com/mozilla/webextension-polyfill/pull/59
+      chrome.pageAction.show(state.tabId);
+    } else {
+      browser.pageAction.show(state.tabId);
+    }
     browser.pageAction.onClicked.addListener(download);
   }
 
@@ -48,11 +47,11 @@ function onReceiveAlbum(album) {
 
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.url.match(/https\:\/\/.*\.bandcamp\.com/)) {
-    console.log('tabs.onUpdated');
     state.album = {};
     state.tabId = tabId;
+    browser.pageAction.onClicked.removeListener(download);
     browser.runtime.onMessage.addListener(onReceiveAlbum);
-    browser.tabs.executeScript({file: "./lib/browser-polyfill.js"});
-    browser.tabs.executeScript({file: "./content.js"});
+    browser.tabs.executeScript({file: path.resolve(__dirname, 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js')});
+    browser.tabs.executeScript({file: path.resolve(__dirname, './dist/content.js')});
   }
 });
